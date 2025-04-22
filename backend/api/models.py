8854721a13d0d4ff831308d3f8c3e9001f03ea28
laura_ay <laura.ay1564@gmail.com>
2025-04-22 15:37:39 -0700

    starting models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Book (models.Model):
+
+class Evidence(models.Model):
+
+class Provenance_Person(models.Model):
+
+class Image (models.Model):
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,8 +5,9 @@ from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
+
