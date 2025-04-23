0c04a6be1d3476cc5c4a0eadd27159c64ee03add
laura_ay <laura.ay1564@gmail.com>
2025-04-22 17:15:59 -0700

    started book model

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Book (models.Model):
+    Title = models.CharField(max_length=250)
+    Author = models.CharField(max_length=250)
+    Pub_Place = models.CharField(max_length=250)
+    Pub_Year = models.CharField(max_length=20)
+    Publisher = models.CharField(max_length=250)
+    Repository = models.CharField(max_length=250)
+    Call_Num = models.CharField(max_length=250)
+    Vol_Num = models.Int
+
+
+
+
 
 class Evidence(models.Model):
 
