8b40177ef795ba803981ad9f3494c6675430f1a7
laura_ay <laura.ay1564@gmail.com>
2025-05-15 16:15:05 -0700

    models almost done

--- backend/api/models.py
+++ backend/api/models.py
@@ -16,7 +16,6 @@ class Book (models.Model):
     catalog_url = repository = models.URLField(blank=True, null=True)
     other_id = models.CharField(max_length=250)
     other_id_type = models.CharField(max_length=250)
-    geo_location = models.CharField(max_length=250)
     sammelband = models.BooleanField(blank=True, null=True)
     acq_source = models.CharField(max_length=250)
 
@@ -37,16 +36,31 @@ class Evidence(models.Model):
     name_associated = models.CharField(max_length=250)
     place_associated = models.CharField(max_length=250)
     citations = models.TextField
+    comment = models.TextField
+    who_id = models.BooleanField(blank=True, null=True)
+    where_id = models.BooleanField(blank=True, null=True)
+    when_id = models.BooleanField(blank=True, null=True)
 
-    
-    
+class Names(models.Model):
+    name = models.CharField(max_length=250)
+    date_start = models.IntegerField(max_length=4)
+    date_end = models.IntegerField(max_length=4)
+    date_description = models.CharField(max_length=250)
+    viaf_id = models.CharField(max_length=250)
+    gender = models.CharField(max_length=250)
+    description = models.TextField
 
+class Places(models.Model):
+    location_name = models.CharField(max_length=250)
+    location_type = models.CharField(max_length=250)
+    lon = models.FloatField()
+    lat = models.FloatField()
 
-
-
-    
-
-
-class Provenance_Person(models.Model): 
+class Provenance_Agent(models.Model): 
+    provenance_role = models.CharField(max_length=250)
+    comment = models.TextField
 
 class Image (models.Model):
+    image_file_name = models.CharField(max_length=250)
+    image_file_size = models.CharField(max_length=250)
+
